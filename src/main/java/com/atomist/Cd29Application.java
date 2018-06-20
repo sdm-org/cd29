8f05aee651c703ed4c8695e6faaaff1ec798522f
Christian Dupuis <cd@atomist.com>
2018-06-20 15:16:51 +0200

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/Cd29Application.java
@@ -0,0 +1,11 @@
+package com.atomist;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cd29Application {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(Cd29Application.class, args);
+	}
+}
---
+++ src/main/java/com/atomist/Cd29Controller.java
@@ -0,0 +1,22 @@
+package com.atomist;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+class Cd29Controller {
+
+    @RequestMapping(method = GET, path = "/")
+    public String root() {
+        return "Hello Kubernetes demo! I'm served by " + getClass().getName();
+    }
+
+    @RequestMapping(method = GET, path = "hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
